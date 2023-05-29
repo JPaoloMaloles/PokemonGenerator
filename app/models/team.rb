bd25f4531e5edb495fea20f7fa99065d7d6e7045
Paolo Maloles <jpaolomaloles@gmail.com>
2023-05-29 13:03:53 -0500

    modified team has_many belongs_to and changed their jbuilder to display pokemon/unique_pokemon

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,5 @@ class Team < ApplicationRecord
   belongs_to :trainer
   has_many :pokemon_in_teams
   has_many :unique_pokemons, through: :pokemon_in_teams
+  has_many :pokemons, through: :unique_pokemons
 end
--- app/views/teams/_team.json.jbuilder
+++ app/views/teams/_team.json.jbuilder
@@ -1,3 +1,5 @@
 json.id team.id
 json.trainer_id team.trainer_id
 json.name team.name
+json.unique_pokemons team.unique_pokemons
+json.pokemons team.pokemons
