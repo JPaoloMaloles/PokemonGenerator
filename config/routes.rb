2953280ee1372bb67123be86dd1e7194ddfd952e
Paolo Maloles <jpaolomaloles@gmail.com>
2023-05-30 15:23:48 -0500

    implemented frontend login, commented out old ways of logging in

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,9 @@ Rails.application.routes.draw do
 
   get "/signup" => "users#new"
   post "/users" => "users#create"
+  get "/login" => "sessions#new"
   post "/sessions" => "sessions#create"
+  get "/logout" => "sessions#destroy"
 
   get "/unique_pokemons/:id", controller: "unique_pokemons", action: "show"
   get "/unique_pokemons", controller: "unique_pokemons", action: "index"
