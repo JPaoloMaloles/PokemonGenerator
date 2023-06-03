134f0413cdcb32f14f3a380503702e47ef598228
Paolo Maloles <jpaolomaloles@gmail.com>
2023-06-03 15:40:13 -0500

    edited navbar to have links to signup/login/logout/pokedex/unique_pokemon some only appearing when logged in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get "/" => "pokemons#index"
   # Defines the root path route ("/")
   # root "articles#index"
   get "/pokemons/:id", controller: "pokemons", action: "show"
