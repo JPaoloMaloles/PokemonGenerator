f4cb1122d10d2c4cd5f1ff5ddc24f4eb7ba64c6d
Paolo Maloles <jpaolomaloles@gmail.com>
2023-06-06 11:48:56 -0500

    modified teams yml to match up with model. commented out old authorization tests in users_controller and sessions_controller, added code in users_controller to test users create action

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,10 +4,19 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  # # This is the old authorization test
+  # test "create" do
+  #   assert_difference "User.count", 1 do
+  #     post "/users.json", params: { name: "Test", email: "test@test.com", password: "password", password_confirmation: "password" }
+  #     assert_response 201
+  #   end
+  # end
+
   test "create" do
     assert_difference "User.count", 1 do
-      post "/users.json", params: { name: "Test", email: "test@test.com", password: "password", password_confirmation: "password" }
-      assert_response 201
+      post "/users", params: { user: { user: 1, name: "Test", email: "test@test.com", password: "password", password_confirmation: "password" } }
+      assert_response 302
     end
   end
 end
