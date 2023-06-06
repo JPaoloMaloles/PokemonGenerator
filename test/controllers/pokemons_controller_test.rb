fff4250026add3628f3bf513c3c27845f78e34df
Paolo Maloles <jpaolomaloles@gmail.com>
2023-06-06 15:23:40 -0500

    created pokemons index test, its not functional because there is no pokemons view html erb file yet

--- test/controllers/pokemons_controller_test.rb
+++ test/controllers/pokemons_controller_test.rb
@@ -4,6 +4,30 @@ class PokemonsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "show" do
+    #puts "this is: #{Pokemon.first.id}"
+    get "/pokemons/#{Pokemon.first.id}"
+    assert_response 200
+
+    # get "/pokemons/#{Pokemon.first.id}.json"
+    # pokemon_hash = JSON.parse(response.body)
+    # assert_equal ["national_dex_num",
+    #               "name",
+    #               "first_type",
+    #               "second_type",
+    #               "abilities",
+    #               "hp",
+    #               "atk",
+    #               "defe",
+    #               "spa",
+    #               "spd",
+    #               "spe",
+    #               "url",
+    #               "icon",
+    #               "first_type_image",
+    #               "second_type_image"], pokemon_hash.keys
+  end
+
   test "index" do
     get "/pokemons"
     assert_response 200
