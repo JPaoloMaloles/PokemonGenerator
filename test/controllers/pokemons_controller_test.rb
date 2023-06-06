142277fdb21d5723bf333221caa7abf47a9c3599
Paolo Maloles <jpaolomaloles@gmail.com>
2023-06-06 15:14:28 -0500

    created pokemon index test

--- test/controllers/pokemons_controller_test.rb
+++ test/controllers/pokemons_controller_test.rb
@@ -4,4 +4,12 @@ class PokemonsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "index" do
+    get "/pokemons"
+    assert_response 200
+
+    get "/pokemons.json"
+    pokemons_hash = JSON.parse(response.body)
+    assert_equal Pokemon.count, pokemons_hash.length
+  end
 end
