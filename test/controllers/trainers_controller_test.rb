35c568cdc978eff258a9b35268030daa4e35c893
Paolo Maloles <jpaolomaloles@gmail.com>
2023-06-11 21:31:48 -0500

    added tests for trainers_controller

--- test/controllers/trainers_controller_test.rb
+++ test/controllers/trainers_controller_test.rb
@@ -1,7 +1,43 @@
 require "test_helper"
 
 class TrainersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = User.create(name: "Test", email: "test@email.com", password: "password")
+    @trainer = Trainer.create(
+      name: "Test Trainer Name",
+      title: "Test Title",
+      level: 0,
+      experience: 0,
+      user_id: @user.id,
+    )
+    @user.update(
+      current_trainer_id: @trainer.id,
+    )
+    @test_pokemon = UniquePokemon.create!(
+      nickname: "nickname",
+      nature: "nature",
+      shiny: "shiny",
+      gender: "gender",
+      hp_ev: 3,
+      atk_ev: 3,
+      defe_ev: 3,
+      spa_ev: 3,
+      spd_ev: 3,
+      spe_ev: 3,
+      hp_iv: 3,
+      atk_iv: 3,
+      defe_iv: 3,
+      spa_iv: 3,
+      spd_iv: 3,
+      spe_iv: 3,
+      trainer_id: @user.current_trainer_id,
+      pokemon_id: Pokemon.all.sample.id,
+    )
+    post "/sessions", params: { email: "test@email.com", password: "password" }
+  end
+
+  test "show" do
+    get "/trainers/#{Trainer.last.id}"
+    assert_response 200
+  end
 end
