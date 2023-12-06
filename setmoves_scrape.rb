0985033abdd4cbe90e0db3d25bf16eda30be68bb
Paolo Maloles <jpaolomaloles@gmail.com>
2023-12-06 17:00:40 -0600

    cleaned up code for setmoves_scrape.rb

--- setmoves_scrape.rb
+++ setmoves_scrape.rb
@@ -24,15 +24,9 @@ MoveProduct = Struct.new(:index, :level_acquire, :name, :type, :category, :pp, :
 
 games.each do |game|
   response = HTTParty.get("https://www.serebii.net/#{game}/#{pokemon_target}/")
-
   document = Nokogiri::HTML(response.body)
-
-  #national_dex_num, name, abilities, hp, atk, def, spa, spd, spe
-
   html_products = document.css("table.dextable")
   html_products = html_products.css("tr")
-
-  
 
   html_products.each do |html_product|
     info = "#{html_product.css("td").text} "
@@ -80,12 +74,9 @@ games.each do |game|
           # puts "AGAIN WE DO THIS: #{info}"
         end
       
-        # if is_move == false
-        #   pokemon_product_index += -1
-        #   next
-        # end
-        pp "info is #{info}"
-        pp "info1 -------------#{info[1]}"
+
+        # pp "info is #{info}"
+        # pp "info1 -------------#{info[1]}"
         if needs_description == false
           if is_egg_move == false
             level_acquire = info[0]
@@ -94,8 +85,6 @@ games.each do |game|
             accuracy = info[3]
             power_points = info[4]
             effect_percent = info[5]
-            # type = info[1]
-            # category = info[1]
           else
             level_acquire = "Egg"
             name = info[0]
@@ -109,57 +98,6 @@ games.each do |game|
         end
 
         if is_move == true && info[1]
-          # power_points = info[1]
-          # power = info[2]
-          # accuracy = info[3]
-
-          # if info[4]
-          #   puts "info1 is #{info[4]}"
-          #   description = info[4].gsub!("Pok", "Poke") || info[4]
-          #   # puts description
-          # end
-
-          # images_url = inforaw.css("img")
-          # # puts images_url
-          # item_image = ""
-          # if !img_check.empty?
-          #   item_image = "https://www.serebii.net" + inforaw.css("img").attribute("src").value
-          #   url = "https://www.serebii.net" + html_product.css("a").first.attribute("href").value
-          # images_url.each do |image_url|
-          #   #puts "This is image_url: #{image_url}"
-          #   #puts "this is image_url.css: #{image_url.attribute("src").value}"
-          #   typeimage << "https://www.serebii.net" + image_url.attribute("src").value
-          # end
-
-          # pp "typeimage is #{typeimage}"
-          # icon = typeimage[0]
-          # first_type_image = typeimage[0]
-          # second_type_image = typeimage[1]
-
-          # #--------- testing parsing out type from substring
-          # first_type_string = first_type_image.match(/(?<=type\/)(.*)(?=(.gif))/).to_s
-          # type = first_type_string
-
-          # if second_type_image
-          #   second_type_string = second_type_image.match(/(?<=type\/)(.*)(?=(.gif))/).to_s
-          #   second_type = second_type_string
-          # end
-          # end
-
-          # # p item_image
-          # # p name
-          # # p description
-
-
-          # img_check = inforaw.css("img")
-          # type_image = ""
-          # if !img_check.empty?
-          #   type_image = "https://www.serebii.net" + img_check[0].attribute("src").value
-          #   category_image = "https://www.serebii.net" + img_check[1].attribute("src").value
-          #   pp type_image
-          #   pp category_image
-          # end
-
           img_check = inforaw.css("img")
           # puts img_check
           if !img_check.empty?
@@ -172,7 +110,6 @@ games.each do |game|
 
             type_url = typeimage[0]
             category_url = typeimage[1]
-            # second_type_url = typeimage[2]
 
             #--------- testing parsing out type from substring
             type_string = type_url.match(/(?<=type\/)(.*)(?=(.gif))/).to_s
@@ -180,22 +117,16 @@ games.each do |game|
 
             pp type_string
             pp category_string
-            # if second_type_image
-            #   second_type_string = second_type_image.match(/(?<=type\/)(.*)(?=(.gif))/).to_s
-            #   second_type = second_type_string
-            # end
           end
 
-          
-          # MoveProduct = Struct.new(:index, :name, :type, :category, :pp, :power, :accuracy,)
           if info.length > 1 && needs_description == false
-            puts "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&"
+            # puts "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&"
             needs_description = true
             next
           end
           pokemon_product = MoveProduct.new(pokemon_product_index, level_acquire, name, type_string, category_string, power_points, power, accuracy, effect_percent, description)
           pokemon_products.push(pokemon_product)
-          puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+          # puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
           needs_description = false
         else
           pokemon_product_index += -1
