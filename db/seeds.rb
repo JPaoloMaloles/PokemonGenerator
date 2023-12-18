691e0d858ba392a5b69174c63e1436c8c14a076f
Paolo Maloles <jpaolomaloles@gmail.com>
2023-12-18 15:03:05 -0600

    added code to seeds.rb that creates Moveset models from scraped information

--- db/seeds.rb
+++ db/seeds.rb
@@ -180,112 +180,112 @@ Team.create!(trainer_id: 1, name: "ExampleTeam2")
 
 url_paths = ["pokeball","holditem","evolutionary","berry","vitamins","fossil","miscellaneous",]
 
-pokemon_product_index = -1
-pokemon_products = []
+# pokemon_product_index = -1
+# pokemon_products = []
 
-ItemProduct = Struct.new(:index, :image, :name, :effect)
+# ItemProduct = Struct.new(:index, :image, :name, :effect)
 
-url_paths.each do |url_path|
-  response = HTTParty.get("https://www.serebii.net/itemdex/list/#{url_path}.shtml")
+# url_paths.each do |url_path|
+#   response = HTTParty.get("https://www.serebii.net/itemdex/list/#{url_path}.shtml")
 
-  document = Nokogiri::HTML(response.body)
+#   document = Nokogiri::HTML(response.body)
 
-  #national_dex_num, name, abilities, hp, atk, def, spa, spd, spe
+#   #national_dex_num, name, abilities, hp, atk, def, spa, spd, spe
 
-  html_products = document.css("table.dextable")
-  html_products = html_products.css("tr")
+#   html_products = document.css("table.dextable")
+#   html_products = html_products.css("tr")
 
 
-  html_products.each do |html_product|
-    info = "#{html_product.css("td").text} "
-    inforaw = html_product.css("td")
-    img_check = inforaw.css("img")
+#   html_products.each do |html_product|
+#     info = "#{html_product.css("td").text} "
+#     inforaw = html_product.css("td")
+#     img_check = inforaw.css("img")
 
-    if !(info.inspect == "\" \"")
-      puts "THIS IS ITERATION--------------------------------------------------------: #{pokemon_product_index}"
-      info = []
-      typeimage = []
-      pokemon_product_index += 1
+#     if !(info.inspect == "\" \"")
+#       puts "THIS IS ITERATION--------------------------------------------------------: #{pokemon_product_index}"
+#       info = []
+#       typeimage = []
+#       pokemon_product_index += 1
 
-      #this code sets the :info attribute of PokemonProduct
-      inforaw.each do |item|
-        tempinfo = []
-        clean_inspect = item.text.inspect.gsub("\\r\\n\\t", "")
-        clean_inspect = clean_inspect.gsub("\\t", "")
-        clean_inspect = clean_inspect.gsub!(/[^0-9A-Za-z.,—\s-]/, "")
+#       #this code sets the :info attribute of PokemonProduct
+#       inforaw.each do |item|
+#         tempinfo = []
+#         clean_inspect = item.text.inspect.gsub("\\r\\n\\t", "")
+#         clean_inspect = clean_inspect.gsub("\\t", "")
+#         clean_inspect = clean_inspect.gsub!(/[^0-9A-Za-z.,—\s-]/, "")
 
-        # puts "this is Clean_Inspect: #{clean_inspect}"
-        tempinfo << clean_inspect
-        # puts "This is tempinfo: #{tempinfo}"
-        tempinfo.each do |item|
-          if item.match?(/[0-9A-Za-z]/)
-            # puts "IT WORKS"
-            info << item
-          end
-        end
-        # puts "AGAIN WE DO THIS: #{info}"
-      end
+#         # puts "this is Clean_Inspect: #{clean_inspect}"
+#         tempinfo << clean_inspect
+#         # puts "This is tempinfo: #{tempinfo}"
+#         tempinfo.each do |item|
+#           if item.match?(/[0-9A-Za-z]/)
+#             # puts "IT WORKS"
+#             info << item
+#           end
+#         end
+#         # puts "AGAIN WE DO THIS: #{info}"
+#       end
     
-      name = info[0]
+#       name = info[0]
 
-      if info[1]
-        # puts "info1 is #{info[1]}"
-        description = info[1].gsub!("Pok", "Poke") || info[1]
-        # puts description
-      end
-
-
-      # puts images_url
-      item_image = ""
-      if !img_check.empty?
-        item_image = "https://www.serebii.net" + inforaw.css("img").attribute("src").value
-      end
-
-      # p item_image
-      # p name
-      # p description
-
-      if name != "Picture"
-        pokemon_product = ItemProduct.new(pokemon_product_index, item_image, name, description)
-        pokemon_products.push(pokemon_product)
-      else
-        pokemon_product_index += -1
-      end
-
-    end
-  end
-end
+#       if info[1]
+#         # puts "info1 is #{info[1]}"
+#         description = info[1].gsub!("Pok", "Poke") || info[1]
+#         # puts description
+#       end
 
 
-csv_headers = ["index", "image", "name", "effect"]
-CSV.open("output.csv", "wb", write_headers: true, headers: csv_headers) do |csv|
-  pokemon_products.each do |pokemon_product|
-    csv << pokemon_product
-  end
-end
+#       # puts images_url
+#       item_image = ""
+#       if !img_check.empty?
+#         item_image = "https://www.serebii.net" + inforaw.css("img").attribute("src").value
+#       end
 
-attribute_titles = ["image", "name", "effect"]
-item_array = []
-count = 0
-pokemon_products.each do |item|
-  single_item = {}
-  count = 0
-  if item.index.to_i > 0 #just for getting a clean list, we can do this in validations later
-    while count < attribute_titles.length
-      single_item[attribute_titles[count].to_sym] = item[count + 1]
-      count += 1
-    end
-    # puts single_item
-    item_array << single_item
-  end
-end
-# pp item_array[0]
-count = 1
-item_array.each do |item|
-  puts "creating item model ##{count}"
-  Item.create(item)
-  count += 1
-end
+#       # p item_image
+#       # p name
+#       # p description
+
+#       if name != "Picture"
+#         pokemon_product = ItemProduct.new(pokemon_product_index, item_image, name, description)
+#         pokemon_products.push(pokemon_product)
+#       else
+#         pokemon_product_index += -1
+#       end
+
+#     end
+#   end
+# end
+
+
+# csv_headers = ["index", "image", "name", "effect"]
+# CSV.open("output.csv", "wb", write_headers: true, headers: csv_headers) do |csv|
+#   pokemon_products.each do |pokemon_product|
+#     csv << pokemon_product
+#   end
+# end
+
+# attribute_titles = ["image", "name", "effect"]
+# item_array = []
+# count = 0
+# pokemon_products.each do |item|
+#   single_item = {}
+#   count = 0
+#   if item.index.to_i > 0 #just for getting a clean list, we can do this in validations later
+#     while count < attribute_titles.length
+#       single_item[attribute_titles[count].to_sym] = item[count + 1]
+#       count += 1
+#     end
+#     # puts single_item
+#     item_array << single_item
+#   end
+# end
+# # pp item_array[0]
+# count = 1
+# item_array.each do |item|
+#   puts "creating item model ##{count}"
+#   Item.create(item)
+#   count += 1
+# end
 
 
 
@@ -536,20 +536,27 @@ all_pokemons.each do |pokemon_model|
             # pp "info is #{info}"
             # pp "info1 -------------#{info[1]}"
             if needs_description == false
-              if is_egg_move == false
+              if is_egg_move == false && is_preev_move == false
                 level_acquire = info[0]
                 name = info[1]
                 power = info[2]
                 accuracy = info[3]
                 power_points = info[4]
                 effect_percent = info[5]
-              else
+              elsif is_egg_move == true
                 level_acquire = "Egg"
                 name = info[0]
                 power = info[1]
                 accuracy = info[2]
                 power_points = info[3]
                 effect_percent = info[4]
+              elsif is_preev_move == true
+                level_acquire = "Pre-Evolution"
+                name = info[0]
+                power = info[3]
+                accuracy = info[4]
+                power_points = info[5]
+                effect_percent = info[6]
               end
             else
               description = info
@@ -597,6 +604,38 @@ all_pokemons.each do |pokemon_model|
         end
       end
     end
+  end
+
+  csv_headers = ["index", "level/acquire","name", "type", "category", "pp", "power", "accuracy", "effect_percent", "description"]
+  CSV.open("output.csv", "wb", write_headers: true, headers: csv_headers) do |csv|
+    pokemon_products.each do |pokemon_product|
+      csv << pokemon_product
+      puts "Added to csv"
+    end
+  end
+
+  attribute_titles = ["pokemon_name","move_name", "level_acquire", "pokemon_id", "move_id"]
+  moveset_array = []
+  count = 0
+  pokemon_products.each do |moveset|
+    single_moveset = {}
+    count = 0
+    if moveset.index.to_i > 0 #just for getting a clean list, we can do this in validations later
+      single_moveset[attribute_titles[count].to_sym] = moveset[count + 1]
+      single_moveset[:pokemon_name] = pokemon_model.name
+      single_moveset[:move_name] = moveset[2]
+      single_moveset[:level_acquire] = moveset[1]
+      single_moveset[:pokemon_id] = Pokemon.find_by(name: pokemon_model.name).id
+      single_moveset[:move_id] = Move.find_by(name: moveset[2]).id
+      moveset_array << single_moveset
+    end
+  end
+  # pp moveset_array[0]
+  count = 1
+  moveset_array.each do |moveset|
+    puts "creating moveset model ##{count}"
+    Moveset.create(moveset)
+    count += 1
   end
 end
 
